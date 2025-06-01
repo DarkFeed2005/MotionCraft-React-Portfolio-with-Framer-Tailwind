44dba1e244e41d4dce72500f359913a93184d0b0
DarkFeed2005 <kalanayasassrisathruwan123@gmail.com>
2025-06-01 17:01:47 +0530

    Import fonts & colours

---
+++ portfolio/tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    colors:{
+            brown: "#53423e",
+            lightbrown: "#d9c6b2",
+            darkbrown: "#2c1f1b",
+            black: "#1e1917",
+            white: "#f1e1d9",
+            cyan: "#15d1e9",
+            lightcyan: "#88e5f0",
+            darkcyan: "#009fb3",
+            orenge: "#fb9718",
+            lightorenge: "#fac27b",
+            darkorenge: "#d28422",
+            gray: "#626965",
+            lightgray: "#978580",
+            drakgray: "#3f4441",
+        },
+
+    extend: {},
+    fontfamily:{ 
+            body: ["Josefin sans"],
+            special: ["Roboto"],
+    },
+  },
+  plugins: [],
+};
+
